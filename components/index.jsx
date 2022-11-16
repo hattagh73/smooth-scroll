2e44faf0c05040ffbbfa0dfc74ff5e9a83fdc87e
hattagh73 <>
2022-11-17 02:01:52 +0800

    add index component

---
+++ components/index.jsx
@@ -0,0 +1,14 @@
+//* Header Component *//
+export { default as Header } from './Header';
+
+//* Btn Component *//
+export { default as BtnGoTop } from './BtnGoTop';
+
+//* Icon Component *//
+export { default as ArrowUp } from './ArrowUp';
+
+//* Section Component *//
+export { default as SectionOne } from './SectionOne';
+export { default as SectionTwo } from './SectionTwo';
+export { default as SectionThree } from './SectionThree';
+export { default as SectionFour } from './SectionFour';
