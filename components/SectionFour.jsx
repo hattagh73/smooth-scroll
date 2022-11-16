0aaad74d65d197508b40dcb5451ac8f4dad15205
hattagh73 <>
2022-11-17 02:02:02 +0800

    add SectionFour component

---
+++ components/SectionFour.jsx
@@ -0,0 +1,9 @@
+
+const SectionFour = () => {
+    return (
+        <section name="sect4" className="flex justify-center items-center">
+            <p className="text-4xl font-semibold">Section Four</p>
+        </section>
+    )
+}
+export default SectionFour;
