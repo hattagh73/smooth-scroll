fda98205759cb3eeca39fe6b9f56654441ab2c23
hattagh73 <>
2022-11-17 02:02:17 +0800

    add SectionThree component

---
+++ components/SectionThree.jsx
@@ -0,0 +1,9 @@
+
+const SectionThree = () => {
+    return (
+        <section name="sect3" className="flex justify-center items-center">
+            <p className="text-4xl font-semibold">Section Three</p>
+        </section>
+    )
+}
+export default SectionThree;
