82cfb6d6d1632010cdfbfafd59cace4bab09598b
hattagh73 <>
2022-11-17 02:02:10 +0800

    add SectionOne component

---
+++ components/SectionOne.jsx
@@ -0,0 +1,9 @@
+
+const SectionOne = () => {
+    return (
+        <section name="sect1" className="flex justify-center items-center">
+            <p className="text-4xl font-semibold">Section One</p>
+        </section>
+    )
+}
+export default SectionOne;
