4abfd5e995d70af1fa14feb7180b8bbec8346f2f
hattagh73 <>
2022-11-17 02:02:26 +0800

    add SectionTwo component

---
+++ components/SectionTwo.jsx
@@ -0,0 +1,9 @@
+
+const SectionTwo = () => {
+    return (
+        <section name="sect2" className="flex justify-center items-center">
+            <p className="text-4xl font-semibold">Section Two</p>
+        </section>
+    )
+}
+export default SectionTwo;
