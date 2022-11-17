69035f22425bdc895bddd3650810eaa0fb25437b
hattagh73 <>
2022-11-17 22:09:21 +0800

    add animation on scrollTo's props
    trying variety of animation scroll and apply on `GO TOP BUTTON`

--- components/BtnGoTop.jsx
+++ components/BtnGoTop.jsx
@@ -1,17 +1,28 @@
 import { useState, useEffect } from 'react';
 import ArrowUp from './ArrowUp';
+import { animateScroll as scroll, scroller } from 'react-scroll';
 
 const BtnGoTop = () => {
 
     const [showScrollIcon, setShowScrollIcon] = useState(false);
 
+    //* Method 1: Scroll to top do manually
     const scrollTop = () => {
         window.scrollTo({
             top: 0,
             behavior: "smooth"
         })
     }
+
+    //* Method 2: Scroll to top using react-scroll | Currently using
+    const scrollToTop = () => {
+        scroll.scrollTo(0, {
+            duration: 1100, //* IF Lesser = Quicker | IF Higher = Slower
+            smooth: 'easeOutSine', //* Some smooth effects based on documentation here: https://easings.net/ and https://www.npmjs.com/package/react-scroll
+        });
+    }
     
+    //* To display icon when scrolling vertically to bottom
     const updateScroll = () => {
         const scroll_position = window.scrollY;
 
@@ -28,7 +39,7 @@ const BtnGoTop = () => {
         <>
             {   showScrollIcon &&
                 <span 
-                    onClick={scrollTop}
+                    onClick={scrollToTop}
                     className="fixed bottom-4 right-4 bg-gray-100 border rounded-full p-[2px]"
                 ><ArrowUp className="fill-black" />
                 </span> 
